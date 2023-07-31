245ec499268cac0c99b26b39fac6f321ad8b09e6
LEESANGNAM <fgh0116@naver.com>
2023-07-31 20:56:10 +0900

    movie 구조체 추가

---
+++ BookWorm/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  BookWorm
+//
+//  Created by 이상남 on 2023/07/31.
+//
+
+import UIKit
+
+
+struct Movie {
+    var title: String
+    var releaseDate: String
+    var runtime: Int
+    var overview: String
+    var rate: Int
+}
