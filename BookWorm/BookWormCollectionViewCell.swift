c744457691a4481c8ad95f180ac2a080d7b8cda6
LEESANGNAM <fgh0116@naver.com>
2023-07-31 20:53:06 +0900

    컬렉션뷰 UI

---
+++ BookWorm/BookWormCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  BookWormCollectionViewCell.swift
+//  BookWorm
+//
+//  Created by 이상남 on 2023/07/31.
+//
+
+import UIKit
+
+class BookWormCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var rateLabel: UILabel!
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
---
+++ BookWorm/BookWormCollectionViewController.swift
@@ -0,0 +1,57 @@
+//
+//  BookWormCollectionViewController.swift
+//  BookWorm
+//
+//  Created by 이상남 on 2023/07/31.
+//
+
+import UIKit
+
+class BookWormCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let nib = UINib(nibName: "BookWormCollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: "BookWormCollectionViewCell")
+        
+        setCollectionViewLayout()
+      
+    }
+    
+    func setCollectionViewLayout(){
+        //cell estimated size non으로 인터페이스 빌더에서 설정 할 것!
+        let layout = UICollectionViewFlowLayout()
+        
+        let spacing: CGFloat = 10
+        let width = UIScreen.main.bounds.width - (spacing * 3)
+        let itemSize = width / 2
+        
+        
+        layout.itemSize = CGSize(width: itemSize, height: itemSize)
+        //컬렉션뷰 inset
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        // 최소 간격
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        collectionView.collectionViewLayout = layout
+        
+        
+    }
+   
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+        return 9
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BookWormCollectionViewCell", for: indexPath) as! BookWormCollectionViewCell
+        cell.backgroundColor = .blue
+        cell.posterImageView.backgroundColor = .red
+    
+        return cell
+    }
+
+
+}
