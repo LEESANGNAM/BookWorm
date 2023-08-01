a51807dd2df0207c3903444b06d500cf526f99e0
LEESANGNAM <fgh0116@naver.com>
2023-08-01 20:18:14 +0900

    상세화면 UI변경

--- BookWorm/DetailViewController.swift
+++ BookWorm/DetailViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 class DetailViewController: UIViewController {
     
     var movie: Movie?
-
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    
     @IBOutlet weak var dateLabel: UILabel!
     
     @IBOutlet weak var timeLabel: UILabel!
@@ -25,16 +26,20 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "상세화면"
+        title = ""
         setUpUI(movie: movie!)
         self.navigationController?.navigationBar.topItem?.title = ""
         // Do any additional setup after loading the view.
     }
     
     func setUpUI(movie: Movie){
-        self.title = movie.title
+        movieTitleLabel.text = movie.title
+        movieTitleLabel.textColor = .white
+        movieTitleLabel.font = .boldSystemFont(ofSize: 20)
         dateLabel.text = movie.releaseDate
+        dateLabel.textColor = .white
         timeLabel.text = "\(movie.runtime)분"
+        timeLabel.textColor = .white
         rateLabel.text = "\(movie.rate)점"
         overViewLabel.text = movie.overview
         posterImageView.image = UIImage(named: movie.title)
