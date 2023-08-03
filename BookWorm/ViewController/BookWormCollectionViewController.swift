17d15129bf2fab001d403913cece444866fe33be
LEESANGNAM <fgh0116@naver.com>
2023-08-03 22:35:14 +0900

    좋아요 버튼기능 동작

--- BookWorm/ViewController/BookWormCollectionViewController.swift
+++ BookWorm/ViewController/BookWormCollectionViewController.swift
@@ -11,12 +11,14 @@ class BookWormCollectionViewController: UICollectionViewController {
     
     let searchBar = UISearchBar()
     
+    var isSearch = false
+    
     var movieList = MovieInfo(){
         didSet{
             collectionView.reloadData()
         }
     }
-    lazy var searchMovieList: [Movie] = movieList.movie {
+    var searchMovieList: [Movie] = []{
         didSet{
             collectionView.reloadData()
         }
@@ -71,14 +73,17 @@ class BookWormCollectionViewController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     
-        return searchMovieList.count
+        return isSearch ? searchMovieList.count : movieList.movie.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BookWormCollectionViewCell.identifier, for: indexPath) as! BookWormCollectionViewCell
-        
-        let movie = searchMovieList[indexPath.row]
-        
+        let movie: Movie
+        if isSearch{
+            movie = searchMovieList[indexPath.row]
+        }else{
+            movie = movieList.movie[indexPath.row]
+        }
         cell.configreCollectionCell(movie: movie)
         
     
@@ -89,18 +94,32 @@ class BookWormCollectionViewController: UICollectionViewController {
     }
     
     @objc func likeButtonTapped(_ sender: UIButton){
-        print("\(sender.tag)")
-        searchMovieList[sender.tag].like.toggle()
-        
+        // 검색 상태 == true
+        if isSearch{
+            // 검색된(검색배열에 담긴)타이틀을 비교를 위해 담아놓는다.
+            let searchTitle = searchMovieList[sender.tag].title
+            // firstindex 클로저로 실행된다. 배열에서 일치하는 title의 인덱스를 가져온다.
+            if let index = movieList.movie.firstIndex(where: { Movie in
+                Movie.title == searchTitle
+            }){ // 버튼의 상태를 바꾼다.
+                searchMovieList[sender.tag].like.toggle()
+                movieList.movie[index].like.toggle()
+            }
+        }else{
+            // 검색상태가 아니라면 원래 있는 배열의 상태를 변경한다.
+            movieList.movie[sender.tag].like.toggle()
+        }
     }
     
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
         guard let vc = storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController else { return }
-        
-        vc.movie = searchMovieList[indexPath.row]
-        
+        if isSearch{
+            vc.movie = searchMovieList[indexPath.row]
+        }else{
+            vc.movie = movieList.movie[indexPath.row]
+        }
         vc.modalTransitionStyle = .coverVertical
         navigationController?.pushViewController(vc, animated: true)
         
@@ -116,27 +135,27 @@ extension BookWormCollectionViewController: UISearchBarDelegate{
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         guard let text = searchBar.text, !text.isEmpty else {
-            resetAllMovie()
+            isSearch = false
             return
         }
+        isSearch = true
         searchMovie(title : text)
     }
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        resetAllMovie()
+        isSearch = false
+        searchBar.text = ""
+        collectionView.reloadData()
     }
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-//        print(searchText)
-//        print(searchBar.text!)
         if searchText.isEmpty {
-            resetAllMovie()
+            isSearch = false
+            collectionView.reloadData()
         }else{
+            isSearch = true
             searchMovie(title : searchText)
         }
     }
-    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-        resetAllMovie()
-    }
-    
+
     // 해당 영화 찾는 함수
     func searchMovie(title : String){
         searchMovieList.removeAll()
@@ -147,21 +166,11 @@ extension BookWormCollectionViewController: UISearchBarDelegate{
         }
         collectionView.reloadData()
     }
-    // 찾는게 없을때 전부 셋팅 함수
-    func resetAllMovie(){
-        searchMovieList.removeAll()
-        searchBar.text = ""
-        for item in movieList.movie {
-            searchMovieList.append(item)
-        }
-        collectionView.reloadData()
-    }
-
 
     func setUpSearchBar(){
         searchBar.delegate = self
         searchBar.placeholder = "검색어를 입력해주세요"
-//        searchBar.showsCancelButton = true
+        searchBar.showsCancelButton = true
         navigationItem.titleView = searchBar
     }
 }
