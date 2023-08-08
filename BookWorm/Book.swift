02c1a65e6ec2b41e757382363a3a647eddd72336
LEESANGNAM <fgh0116@naver.com>
2023-08-09 01:57:03 +0900

    Book추가

---
+++ BookWorm/Book.swift
@@ -0,0 +1,20 @@
+//
+//  Book.swift
+//  BookWorm
+//
+//  Created by 이상남 on 2023/08/09.
+//
+
+import Foundation
+
+struct Book{
+    var title: String
+    var releaseDate: String
+    var runtime: Int
+    var overview: String
+    var rate: Double
+    var like: Bool
+    var color: UIColor
+}
+
+
